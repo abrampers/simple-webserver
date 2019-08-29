bd68fd9ce5aa280742dbba3c6a2e4e43a209c14b
Abram Situmorang <abram.perdanaputra@gmail.com>
2019-08-29 18:28:46 +0700

    Finished webserver

---
+++ c++/CMakeFiles/simple-webserver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple-webserver.dir/main.cpp.o"
+  "simple-webserver.pdb"
+  "simple-webserver"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple-webserver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

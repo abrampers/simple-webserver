5d55584a164c10236fc2496ac13bf7f210a6e011
Restu Wahyu Kartiko <13516155@std.stei.itb.ac.id>
2019-09-04 14:42:20 +0700

    Add k6 script

---
+++ k6_script.js
@@ -0,0 +1,8 @@
+import http from "k6/http";
+import { sleep } from "k6";
+
+export default function() {
+  // http.get("http://localhost/test20KB.html");
+  http.get("http://localhost:3000");
+  // sleep(1);
+};
